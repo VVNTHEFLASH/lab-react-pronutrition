cebef1dc2316fc170b3c9ac459ef47fd4eb874a4
VVNTHEFLASH <vishnurockhunters@gmail.com>
2021-10-23 01:02:45 +0530

    Progression 1 Done

---
+++ pronutrition/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import FoodBox from './components/FoodBox/FoodBox';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='head-bar'>
+        <h3>Pro-Nutritions</h3>
+      </div>
+      <FoodBox />
+    </div>
+  );
+}
+
+export default App;
